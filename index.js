fa44b6d691df989cf83c758bed9b144472ff0cd7
Charlie van Rantwijk <charlievanrantwijk@gmail.com>
2018-12-17 14:37:00 +0100

    more functions added

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 var sovrinDID = require("sovrin-did");
 
-var d = sovrinDID.gen();
+//genDIDs(10);
 
-console.log(d);
+testNonce();
+
+function genDIDs(amount){
+    for(let i=0;i<amount;i++){
+        console.log(sovrinDID.gen());
+        console.log('\n');
+    }
+}
+
+function sign(){
+    let sovrin = sovrinDID.gen();
+    var signKey = sovrin.secret.signKey;
+    var verifyKey = sovrin.verifyKey;
+    var message = "Hello World!!";
+
+    var signedMessage = sovrinDID.signMessage(message, signKey, verifyKey);
+
+    console.log(sovrinDID.verifySignedMessage(signedMessage, verifyKey));
+}
+
+function getKeyPair(){
+    let sovrin = sovrinDID.gen();
+    var signKey = sovrin.secret.signKey;
+
+    var keyPair = sovrinDID.getKeyPairFromSignKey(signKey);
+    console.log(keyPair);
+}
+
+function testNonce(){
+    let nonce = sovrinDID.getNonce();
+    console.log(String.fromCharCode.apply(null, nonce));
+}
